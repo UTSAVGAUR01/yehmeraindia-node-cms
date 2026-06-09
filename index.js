1e55ff56fc617102b7cd207ec9da6052c182ffe9
Utsav Gour <139868417+UTSAVGAUR01@users.noreply.github.com>
2026-06-09 11:32:15 +0530

    Remove runtime build from startup

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-import fs from 'fs';
-import { execSync } from 'child_process';
-
-if (!fs.existsSync('./dist/index.html')) {
-  console.log('dist/index.html not found. Building frontend before starting server...');
-  execSync('npm run build', { stdio: 'inherit' });
-}
-
 await import('./server/index.js');
