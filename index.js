2365dd41d3ba45a876fe14fa08a9ef3d84e816e8
Utsav Gour <139868417+UTSAVGAUR01@users.noreply.github.com>
2026-07-14 18:28:26 +0530

    load verified Viewer journal message gate

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import "./server/frontend-cache-preload.js";
 import "./server/security-edge-preload.js";
 import "./server/viewer-verification-preload.js";
+import "./server/viewer-message-gate-preload.js";
 import "./server/bilingual-place-preload.js";
 import "./server/place-research-normalize-preload.js";
 import "./server/book-collation-fix.js";
