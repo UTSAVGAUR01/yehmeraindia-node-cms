926e81690ce686066882a907cb216aea501a9f8d
Utsav Gour <139868417+UTSAVGAUR01@users.noreply.github.com>
2026-07-15 01:34:59 +0530

    refresh cached HD map and place language fixes

--- public/ymi-sw.js
+++ public/ymi-sw.js
@@ -1,4 +1,4 @@
-const VERSION = "ymi-vpn-shell-6";
+const VERSION = "ymi-vpn-shell-7";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const TILE_CACHE = `${VERSION}-tiles`;
