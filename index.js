0b04913ba8c2486f5d921afe1b3ab2cfe229d2d1
Utsav Gour <139868417+UTSAVGAUR01@users.noreply.github.com>
2026-07-14 17:14:23 +0530

    load footer and author social APIs before CMS

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import "./server/footer-email-preload.js";
 import "./server/security-auth-preload.js";
 import "./server/member-studio-preload.js";
 import "./server/profile-preload.js";
+import "./server/social-footer-preload.js";
 import "./server/index.js";
