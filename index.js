793621690327e2cf3d0ed10475ed0b76efa46016
Utsav Gour <139868417+UTSAVGAUR01@users.noreply.github.com>
2026-07-14 15:48:43 +0530

    load member studio and OTP reset routes before CMS startup

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 import "./server/book-collation-fix.js";
 import "./server/hostinger-preload.js";
 import "./server/security-auth-preload.js";
+import "./server/member-studio-preload.js";
 import "./server/index.js";
