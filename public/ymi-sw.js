1db7892f64cfb31c0adbac5b573084dc826b9b01
Utsav Gour <139868417+UTSAVGAUR01@users.noreply.github.com>
2026-07-15 18:04:20 +0530

    Refresh cached journal image styling

--- public/ymi-sw.js
+++ public/ymi-sw.js
@@ -1,4 +1,4 @@
-const VERSION = "ymi-vpn-shell-13";
+const VERSION = "ymi-vpn-shell-14";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const TILE_CACHE = `${VERSION}-tiles`;
