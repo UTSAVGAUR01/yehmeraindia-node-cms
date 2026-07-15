be71f898cc6133c7b4dc77ae5f296941ea05efa6
Utsav Gour <139868417+UTSAVGAUR01@users.noreply.github.com>
2026-07-15 16:23:58 +0530

    Refresh service worker cache for catalogue AI writer

--- public/ymi-sw.js
+++ public/ymi-sw.js
@@ -1,4 +1,4 @@
-const VERSION = "ymi-vpn-shell-9";
+const VERSION = "ymi-vpn-shell-10";
 const SHELL_CACHE = `${VERSION}-shell`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const TILE_CACHE = `${VERSION}-tiles`;
